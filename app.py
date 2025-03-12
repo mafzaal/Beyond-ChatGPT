302be5dfae7cef47b23c291a70e21a8d9aea5bd3
Muhammad Afzaal <mafzaal@gmail.com>
2025-03-12 18:25:25 -0500

    Refactor requirements and enhance message handling in app.py; add translations for UI elements

--- app.py
+++ app.py
@@ -4,8 +4,7 @@
 import os
 from openai import AsyncOpenAI  # importing openai for API usage
 import chainlit as cl  # importing chainlit for our app
-from chainlit.prompt import Prompt, PromptMessage  # importing prompt tools
-from chainlit.playground.providers import ChatOpenAI  # importing ChatOpenAI tools
+# from chainlit.types import Prompt, PromptMessage  # importing prompt tools
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -18,6 +17,21 @@ user_template = """{input}
 Think through your response step by step.
 """
 
+def system_message(content):
+    return {
+        "role": "system",
+        "content": content,
+    }
+
+def user_message(content):
+    
+    return {
+        "role": "user",
+        "content": user_template.format(input=content),
+    }
+
+
+
 
 @cl.on_chat_start  # marks a function that will be executed at the start of a user session
 async def start_chat():
@@ -28,6 +42,7 @@ async def start_chat():
         "top_p": 1,
         "frequency_penalty": 0,
         "presence_penalty": 0,
+
     }
 
     cl.user_session.set("settings", settings)
@@ -41,40 +56,18 @@ async def main(message: cl.Message):
 
     print(message.content)
 
-    prompt = Prompt(
-        provider=ChatOpenAI.id,
-        messages=[
-            PromptMessage(
-                role="system",
-                template=system_template,
-                formatted=system_template,
-            ),
-            PromptMessage(
-                role="user",
-                template=user_template,
-                formatted=user_template.format(input=message.content),
-            ),
-        ],
-        inputs={"input": message.content},
-        settings=settings,
-    )
-
-    print([m.to_openai() for m in prompt.messages])
-
+   
     msg = cl.Message(content="")
 
     # Call OpenAI
     async for stream_resp in await client.chat.completions.create(
-        messages=[m.to_openai() for m in prompt.messages], stream=True, **settings
+        messages=[system_message(system_template),user_message(content=message.content)], stream=True, **settings
     ):
         token = stream_resp.choices[0].delta.content
         if not token:
             token = ""
         await msg.stream_token(token)
 
-    # Update the prompt object with the completion
-    prompt.completion = msg.content
-    msg.prompt = prompt
-
+   
     # Send and close the message stream
     await msg.send()
